fc66b0d5d0cb91e8ace5189593db1c0ec68fa506
TrinhNgocToan <tntoan.24174600119@sv.uneti.edu.vn>
2024-10-23 14:34:44 +0700

    lưu thay đổi

--- python/chuong_6/bai_10.py
+++ python/chuong_6/bai_10.py
@@ -1,5 +1,8 @@
 import math 
-a = float(input("NHẬP GIA TỐC CỦA XE LÀ: "))
-t = float(input("THỜI GIAN XE CHẠY LÀ: "))
-v = - t * math.log(5) / math.log(4) * a ** 4
-print(f"Vận chuyển động là: {v:.2f}")
+v = float(input("Nhập vận tốc a của xe ô tô (m/s): "))
+if v <= 0:
+    print ("vận tốc không thể âm hoặc bằng 0:")
+else:
+    math.log(4,5) != 0
+    t = v**4 / math.log(4,5)
+    print(f"thời gian từ lúc đạp phanh đến lúc dừng(giây): {t:.2f}")
